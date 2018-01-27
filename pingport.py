87c4bacbd93f181881c0696b3b4f858068541e0b
shadowfox <cuiyingjia@foxmail.com>
2018-01-28 00:04:00 +0800

    py3

--- pingport.py
+++ pingport.py
@@ -5,10 +5,12 @@ def IsAvailable(ip,port):
     try:
         s.connect((ip,int(port)))
         s.shutdown(2)
-        print '%d available~' % port
+        # print '%d available~' % port #py2
+        print ('%d available~' % port) #py3
         return True
     except:
-        print '%d is busy!' % port
+        # print '%d is busy!' % port #py2
+        print ('%d is busy!' % port) #py3
         return False
 if __name__ == '__main__':
     IsAvailable('127.0.0.1',8080)#input here
