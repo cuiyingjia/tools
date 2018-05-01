afdc17bf72e6494d27d6d8c49fbae80e59c648bb
cuiyingjia <cuiyingjia@foxmail.com>
2018-05-01 22:55:57 +0800

    list file name

--- py3filerename.py
+++ py3filerename.py
@@ -1,21 +1,18 @@
 import os
-path="C://Users//cuiyi//Downloads//MA//TEST"
+FILE_PATH="C://Users//cuiyi//Downloads//MA//TEST"
 # for i in os.walk(path):
 # 	print(i) 
 fileLists = []
-def myTotalSize(path):
-    memory = 0
+def renameFile(path):
     fileList = os.listdir(path)
     for file in fileList:
         newPath = os.path.join(path, file)  
         # print("======="+newPath)
         if os.path.isdir(newPath):
-            memory += myTotalSize(newPath) 
+            renameFile(newPath) 
         elif os.path.isfile(newPath):
             if os.path.splitext(newPath)[1].upper() == '.LOG':
                 fileLists.append(os.path.basename(newPath))
-            memory += os.path.getsize(newPath)
-    return memory
 
-print(myTotalSize(path))
+renameFile(FILE_PATH)
 print(fileLists)
