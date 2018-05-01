646365de39cf40a815955c8d6e0c7827e9273908
cuiyingjia <cuiyingjia@foxmail.com>
2018-05-01 23:17:42 +0800

    find the file suffix by regex

--- py3filerename.py
+++ py3filerename.py
@@ -1,8 +1,11 @@
 import os
+import re
+
 FILE_PATH="C://Users//cuiyi//Downloads//MA//TEST"
 # for i in os.walk(path):
 # 	print(i) 
 fileLists = []
+pattern = re.compile(r'.\d')
 def renameFile(path):
     fileList = os.listdir(path)
     for file in fileList:
@@ -11,8 +14,14 @@ def renameFile(path):
         if os.path.isdir(newPath):
             renameFile(newPath) 
         elif os.path.isfile(newPath):
-            if os.path.splitext(newPath)[1].upper() == '.LOG':
+            filename=os.path.splitext(newPath)[1].upper()
+            print(filename)
+            match = pattern.match(filename)
+            print(match)
+            if match:
+                print("OLD:====="+newPath)
                 fileLists.append(os.path.basename(newPath))
+            
 
 renameFile(FILE_PATH)
 print(fileLists)
